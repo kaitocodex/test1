9ef369ff319f14dcfd501aa590908a46fff6148b
kaitocodex <kaitoware@gmail.com>
2024-10-03 02:37:49 +0530

    updatetest

--- indexlocal.js
+++ indexlocal.js
@@ -3,7 +3,6 @@ function runTest() {
     console.log("Running test...");
     const sum = (a, b) => a + b;
     console.log("Sum of 2 and 3 is:", sum(2, 3));
-    //git remote set-url origin git@github.com:kaitocodex/testingGit.git
 }
 
 runTest(); 
